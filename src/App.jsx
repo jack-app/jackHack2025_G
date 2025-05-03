3904d30bd3495a2c1210df47975434e29e968174
nagoyayuu15 <nagoyayuu15@gmail.com>
2025-05-03 22:49:09 +0900

    動作確認

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-class Game {
-  constructor() {
-
-  }
-
-  
-}
+class Game {}
 
 function App() {
   const [count, setCount] = useState(0)
@@ -34,7 +28,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Deploy was successful.
       </p>
     </>
   )
