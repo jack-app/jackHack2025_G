4da421a2915866d7d19cd0d5bfaf4bfc9a25593c
nagoyayuu15 <nagoyayuu15@gmail.com>
2025-05-03 22:46:53 +0900

    設定更新

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'docs'
-  },
   base: 'https://jack-app.github.io/jackHack2025_G/'
 })
