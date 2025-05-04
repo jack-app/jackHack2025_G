bc21d4490b7bbfe79220e65cba46c1f98e49aff5
nagoyayuu15 <nagoyayuu15@gmail.com>
2025-05-04 22:06:01 +0900

    ファイル分割

--- src/start_page/main.jsx
+++ src/start_page/main.jsx
@@ -3,6 +3,8 @@ import "./main.css"
 import windows_logo from "./windows.jpg"
 
 import { GameSetUp } from "../messengers"
+import UserSelectArea from './user_select_area'
+import DifficultySelector from './difficulty_selector'
 
 class StartPageHandler {
     constructor(gameSetup) {
@@ -53,58 +55,6 @@ function StartPageContentRoot(props) {
         <div id="start-footer">
             <DifficultySelector handler={props.handler}/>
         </div>   
-    </div>
-}
-
-import "./user-select-area.css"
-import button_image from "./button-dummy.png"
-
-function UserSelectArea(props) {
-    return <div class="user-select-area">
-        <div class="display-box">
-            <div class="user-icon"></div>
-            <div class="letters">
-                <p>Player</p>
-                <p>ユーザー名の入力</p>
-            </div>
-        </div>
-        <div class="input-box">
-            <input onChange={(element) => props.handler.updateUserName(element.target.value)}/>
-            <img src={button_image} onClick={() => props.handler.startGame()}/>
-        </div>
-    </div>
-}
-
-function DifficultySelector(props) {
-    return <div id="difficulty_selector">
-        <label>
-            <input 
-                onChange={(element) => 
-                    props.handler.updateDifficulty(element.target.value)
-                } 
-                type="radio" name="difficulty" value="easy" id="easy"
-            />
-            easy
-        </label>
-        <label>
-            <input 
-                onChange={(element) => 
-                    props.handler.updateDifficulty(element.target.value)
-                } 
-                type="radio" name="difficulty" value="normal" id="normal"
-                checked
-            />
-            normal
-        </label>
-        <label>
-            <input 
-                onChange={(element) => 
-                    props.handler.updateDifficulty(element.target.value)
-                } 
-                type="radio" name="difficulty" value="hard" id="hard"
-            />
-            hard
-        </label>
     </div>
 }
 
