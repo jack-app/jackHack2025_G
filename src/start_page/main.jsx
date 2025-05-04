d87fc20f2eae0a1466d5e9095c228dd8fa1d22af
gata2825 <FuYa7226977@outlook.jp>
2025-05-04 16:03:05 +0900

    スタートページの見た目を作った

--- src/start_page/main.jsx
+++ src/start_page/main.jsx
@@ -1,7 +1,62 @@
+import { useState } from "react";
+import "./main.css"
+
 function StartPage(props) {
-    return <>
-        <h1>StartPage</h1>
-        <button onClick={() => props.handler.startGame()}>Start Game</button>
-    </>
+    const [difficulty, setDifficulty] = useState("normal")
+    return <div id="start-page-container">
+        <div id="start-header"></div>
+
+        <div id="start-body-up-line"></div>
+
+        <div id="start-body">
+            <div id="start-body-left">
+                <h1>windows x<br />explain</h1>
+            </div>
+            <div id="start-body-right">
+                <input onChange={(element) => props.handler.userNameUpdate(element.target.value)}></input>
+                <button onClick={() => props.handler.startGame()}>Start Game</button>
+            </div>
+        </div>
+
+        <div id="start-body-bottom-line"></div>
+
+        <div id="start-footer">
+            <div id="difficulty_selector">
+                <label>
+                    <input 
+                        onChange={(element) => {
+                            props.handler.difficultyUpdate(element.target.value)
+                            setDifficulty(element.target.value)
+                        }} 
+                        type="radio" name="difficulty" value="easy"
+                        checked={difficulty === "easy"}
+                    />
+                    easy
+                </label>
+                <label>
+                    <input 
+                        onChange={(element) => {
+                            props.handler.difficultyUpdate(element.target.value)
+                            setDifficulty(element.target.value)
+                        }} 
+                        type="radio" name="difficulty" value="normal"
+                        checked={difficulty === "normal"}
+                    />
+                    normal
+                </label>
+                <label>
+                    <input 
+                        onChange={(element) => {
+                            props.handler.difficultyUpdate(element.target.value)
+                            setDifficulty(element.target.value)
+                        }} 
+                        type="radio" name="difficulty" value="hard"
+                        checked={difficulty === "hard"}
+                    />
+                    hard
+                </label>
+            </div>
+        </div>   
+    </div>
 }
 export default StartPage;
