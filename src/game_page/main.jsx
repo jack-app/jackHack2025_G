c20781f8e220e997460682c3431913301a634cc7
Pierre-Emmanuel Wulfman <pierre.emmanuel.wulfman@gmail.com>
2025-05-05 13:47:21 +0900

    add windows bar

--- src/game_page/main.jsx
+++ src/game_page/main.jsx
@@ -1,56 +1,75 @@
-import { h, Fragment } from 'start-dom-jsx' // JSXを使うためのおまじない
+import { h, Fragment } from 'start-dom-jsx'; // JSXを使うためのおまじない
 
 import { GameSetUp, GameResult } from "../messengers";
 import GameController from "./gameController";
 
+import StartButton from "./start.png";
+import SoundIcon from "./sound-icon.png";
+import GuardIcon from "./guard-icon.png";
 import './main.css';
 
 class GamePageHandler {
-  constructor(gameSetup) {
+   constructor( gameSetup ) {
       this.gameSetup = gameSetup ?? new GameSetUp();
       this.gameResult = new GameResult();
       this.watingGameEnd = [];
       this.popUpWindowManager = null;
-  }
+   }
 
-  addGameEndListener(listener) {
-      this.watingGameEnd.push(listener);
-  }
+   addGameEndListener( listener ) {
+      this.watingGameEnd.push( listener );
+   }
 
-  waitForGameEnd() {
+   waitForGameEnd() {
       // resolveが呼ばれるまで待機する．resolveはendGameが呼ばれたときに呼ばれる
-      return new Promise((resolve) => {
-          this.watingGameEnd.push(resolve);
-      });
-  }
+      return new Promise( ( resolve ) => {
+         this.watingGameEnd.push( resolve );
+      } );
+   }
 
-  endGame() {
-      this.watingGameEnd.forEach((resolve) => resolve());
+   endGame() {
+      this.watingGameEnd.forEach( ( resolve ) => resolve() );
       this.watingGameEnd = [];
-  }
+   }
 }
 
-function GamePageContentRoot(props) {
-  const windowContainer = <div id="game-main-field"></div>
+function GamePageContentRoot( props ) {
+   const windowContainer = <div id="game-main-field"></div>;
 
-  const game_controller = new GameController(
-    props.handler.gameSetup,
-    props.handler.gameResult,
-    windowContainer,
-    () => props.handler.endGame()
-  );
-  
-  const content = <div id="game-page-container">
-    {windowContainer}
-  </div>;
+   const game_controller = new GameController(
+      props.handler.gameSetup,
+      props.handler.gameResult,
+      windowContainer,
+      () => props.handler.endGame()
+   );
 
-  game_controller.start()
+   const content = <div id="game-page-container">
+      { windowContainer }
+      <footer class='window-bar'>
+         <div class="window-bar-item left">
+            <img src={ StartButton } />
+         </div>
+         <div class="window-bar-item center">
+            <div class="skill-bar">
+            </div>
+         </div>
+         <div class="window-bar-item right">
+            <img class="footer-icon" src={ SoundIcon } />
+            <img class="footer-icon" src={ GuardIcon } />
+            {/* TODO : display elapse time  */ }
+            <div class="time">11:22 AM </div>
 
-  return content;
+         </div>
+      </footer>
+   </div>;
+
+   game_controller.start();
+
+   return content;
 }
 
 const Game = {
-  handler: GamePageHandler,
-  content: GamePageContentRoot,
-}
+   handler: GamePageHandler,
+   content: GamePageContentRoot,
+};
 export default Game;
