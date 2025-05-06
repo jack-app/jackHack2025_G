b0962bb207cf672f6c957de3c8d6b0150349a91b
potato <yakisobanotare@gmail.com>
2025-05-06 12:31:56 +0900

    広告画像を増やした

--- src/game_page/popups/popup_contents.jsx
+++ src/game_page/popups/popup_contents.jsx
@@ -1,12 +1,29 @@
 import { h, Fragment } from 'start-dom-jsx' // JSXを使うためのおまじない
+import "./popupcontents-potato.css"
+import { defaultWindowHeight, defaultWindowWidth } from './const';
 
 export function RandomContent() {
     const options = [
         <SampleContent/>,
+        <Virus/>,
+        <Coffee/>,
+        <Uranai/>
     ]
     return options[Math.floor(Math.random() * options.length)];
 }
 
 function SampleContent() {
     return <p>Sample Content</p>
+}
+
+function Virus() {
+    return <img src="src/game_page/popups/asset/meiwakugazou.png" width={defaultWindowWidth} height={defaultWindowHeight} />
+}
+
+function Coffee() {
+    return <img src="src/game_page/popups/asset/cancoffee.png" width={defaultWindowWidth} height={defaultWindowHeight} />
+}
+
+function Uranai() {
+    return <img src="src/game_page/popups/asset/uranai.png" width={defaultWindowWidth} height={defaultWindowHeight} />
 }
